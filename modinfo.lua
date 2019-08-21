c186c05975da65fda0a1f3a06ec5300170776e6a
skymyj <skymyj@hotmail.com>
2019-08-21 17:48:27 +0200

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,10 @@
+return {
+  name='Tech Annihilation',
+  description='The New Generation Of War',
+  shortname='TA',
+  version='$VERSION',
+  mutator='',
+  game='',
+  shortGame='TA',
+  modtype=1,
+  }
